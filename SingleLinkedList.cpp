60a15f3b16b868de1971cdfa1587e7972e9f9847
Fanshi13 <aquillafazanaufan@gmail.com>
2025-05-08 19:44:05 +0700

    commit 3

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -20,4 +20,16 @@ public:
     }
 
     void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+                }
+    }
 }
